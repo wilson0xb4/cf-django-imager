ce9dce13f8df37a253cf63c9c6f07f39cc91b764
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-27 09:38:42 -0700

    add Photo model

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,18 +1,50 @@
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible
 class Photo(models.Model):
-    pass
+    PUBLISHED_CHOICES = (
+        ('private', 'private'),
+        ('shared', 'shared'),
+        ('public', 'public')
+    )
+    user = models.ForeignKey(
+        User,
+        null=False
+    )
+    image = models.ImageField(
+        upload_to='photo_files/%Y-%m-%d'
+    )
+    title = models.CharField(
+        max_length=200
+    )
+    description = models.TextField(
+        blank=True
+    )
+    date_uploaded = models.DateField(
+        auto_now_add=True
+    )
+    date_modified = models.DateField(
+        auto_now=True
+    )
+    date_published = models.DateField(
+        null=True,
+        blank=True
+    )
+    published = models.CharField(
+        max_length=200,
+        choices=PUBLISHED_CHOICES,
+        default='private'
+    )
 
-    def __str__():
-        pass
+    def __str__(self):
+        return self.title
 
 
 @python_2_unicode_compatible
 class Album(models.Model):
-    pass
 
     def __str__():
         pass

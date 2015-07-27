8af3b2aa4f595fe06a45c04fde41e78e50150e5f
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-27 08:50:35 -0700

    shell of images model

--- imagersite/imager_images/models.py
+++ imagersite/imager_images/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Photo(models.Model):
+    pass
+
+    def __str__():
+        pass
+
+
+@python_2_unicode_compatible
+class Album(models.Model):
+    pass
+
+    def __str__():
+        pass

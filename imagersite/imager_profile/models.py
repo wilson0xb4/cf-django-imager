d7c9d81c79c584a9380d011826e1a446fb881582
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-07-27 00:07:49 -0700

    create basic profile model and register with admin

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class ImagerProfile(models.Model):
+    camera = models.CharField(max_length=200)
+    address = models.TextField(max_length=200)
+    website = models.URLField()
+    photography_type = models.CharField(max_length=200)
+
+    def __str__():
+        pass

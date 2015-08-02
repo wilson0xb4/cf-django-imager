0933d5088198c598779d02cbd8460d0983dfbe72
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-08-02 16:14:39 -0700

    install sorl-thumbnail

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = (
     'bootstrap3',
     'registration',
+    'sorl.thumbnail',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

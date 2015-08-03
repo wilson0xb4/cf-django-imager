9c4c891a1468732518625ba933dbd3b67d8d0358
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-08-02 21:59:37 -0700

    add urls and views for albums and photos

--- imagersite/imager_images/views.py
+++ imagersite/imager_images/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
+from imager_images.models import Album, Photo
 
-# Create your views here.
+
+class AlbumView(TemplateView):
+    template_name = 'album.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(AlbumView, self).get_context_data(**kwargs)
+
+        try:
+            context['user'] = self.request.user
+            album_set = self.request.user.album_set
+            context['album'] = album_set.get(id=kwargs.get('pk'))
+        except Album.DoesNotExist:
+            pass
+
+        return context
+
+
+class PhotoView(TemplateView):
+    template_name = 'photo.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(PhotoView, self).get_context_data(**kwargs)
+
+        try:
+            context['user'] = self.request.user
+            photo_set = self.request.user.photo_set
+            context['photo'] = photo_set.get(id=kwargs.get('pk'))
+        except Photo.DoesNotExist:
+            pass
+
+        return context

192e8d5c80ce80810eed9d0ed0589e798250bf08
Andrew Wilson <andrew.wilsonx@gmail.com>
2015-08-17 14:41:53 -0700

    change static/media roots

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -113,7 +113,7 @@ STATIC_URL = '/static/'
 
 # Media file handling
 MEDIA_ROOT = os.environ.get(
-    'IMAGER_MEDIA_ROOT',
+    'MEDIA_ROOT',
     os.path.join(BASE_DIR, 'media')
 )
 MEDIA_URL = '/media/'

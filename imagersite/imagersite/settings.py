3c32651fe1c17379c75c5502b23635973eaa0116
johnsonjew <johnsonjjew@gmail.com>
2015-08-18 16:34:40 -0700

    removed debug

--- imagersite/imagersite/settings.py
+++ imagersite/imagersite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', None)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', True)
+DEBUG = os.environ.get('DEBUG', False)
 TEMPLATE_DEBUG = DEBUG
 THUMBNAIL_DEBUG = DEBUG
 
